b1da8a957490e35423d2800c4aa0ba851b87ad6f
Younes Bellakridi <ybellakr@e3r4p14.1337.ma>
2025-04-28 10:17:39 +0100

    add some fixes to ex02

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -1,8 +1,48 @@
 #include "PmergeMe.hpp"
-#include <algorithm>
-#include <cstdlib>
+#include <iterator>
+#include <ostream>
 
-//check if there is any double return ;
+int PmergeMe::check_double(int ac, char **av)
+{
+    for (size_t i = 0; i < pairs.size(); i++)
+    {
+        if (pairs[i].first == pairs[i].second)
+            return (1);
+        for (size_t j = 0; j < pairs.size(); j++)
+        {
+            if (j != i && (pairs[i].first == pairs[j].first || pairs[i].first == pairs[j].second ||
+                pairs[i].second == pairs[j].first || pairs[i].second == pairs[j].second))
+                return 1;
+            if (ac - 1 > ((int)pairs.size() * 2))
+            {
+                if (j != i && (pairs[i].first == atoi(av[ac-1]) || pairs[i].second == atoi(av[ac-1])))
+                    return (1);
+            }
+        }
+    }
+    return (0);
+}
+
+int PmergeMe::check_double_dq(int ac, char **av)
+{
+    for (size_t i = 0; i < pairs_dq.size(); i++)
+    {
+        if (pairs_dq[i].first == pairs_dq[i].second)
+            return (1);
+        for (size_t j = 0; j < pairs_dq.size(); j++)
+        {
+            if (j != i && (pairs_dq[i].first == pairs_dq[j].first || pairs_dq[i].first == pairs_dq[j].second ||
+                pairs_dq[i].second == pairs_dq[j].first || pairs_dq[i].second == pairs_dq[j].second))
+                return 1;
+            if (ac - 1 > ((int)pairs_dq.size() * 2))
+            {
+                if (j != i && (pairs_dq[i].first == atoi(av[ac-1]) || pairs_dq[i].second == atoi(av[ac-1])))
+                    return (1);
+            }
+        }
+    }
+    return (0);
+}
 
 int PmergeMe::check(char **av)
 {
@@ -31,6 +71,16 @@ void PmergeMe::sort_inside_pair()
 		{
 			std::swap(this->pairs[i].first, this->pairs[i].second);
 		}
+	}
+}
+
+
+void PmergeMe::sort_inside_pair_dq()
+{
+	for (int i = 0; i < (int)this->pairs_dq.size(); i ++)
+	{
+		if (this->pairs_dq[i].first < this->pairs_dq[i].second)
+			std::swap(this->pairs_dq[i].first, this->pairs_dq[i].second);
 	}
 }
 
@@ -64,6 +114,49 @@ void PmergeMe::merge(int left, int mid, int right)
         pairs[i] = temp[i - left];
 }
 
+void PmergeMe::merge_dq(int left, int mid, int right)
+{
+    std::deque<std::pair<int, int> > temp;
+    int low = left;
+    int high = mid + 1;
+
+    while (low <= mid && high <= right)
+    {
+        if (pairs_dq[low] <= pairs_dq[high])
+        {
+            temp.push_back(pairs_dq[low]);
+            low++;
+        }
+        else 
+        {
+            temp.push_back(pairs_dq[high]);
+            high++;
+        }
+    }
+
+    while (low <= mid) 
+        temp.push_back(pairs_dq[low++]);
+
+    while (high <= right) 
+        temp.push_back(pairs_dq[high++]);
+
+    for (int i = left; i <= right; i++)
+        pairs_dq[i] = temp[i - left];
+}
+
+
+
+void PmergeMe::helper_merge_dq(int left, int right)
+{
+    if (right > left)
+    {
+        int mid = (left + right) / 2;
+        this->helper_merge_dq(left, mid);
+        this->helper_merge_dq(mid + 1, right);
+        this->merge_dq(left, mid, right);
+    }
+}
+
 void PmergeMe::helper_merge(int left, int right)
 {
     if (right > left)
@@ -73,6 +166,18 @@ void PmergeMe::helper_merge(int left, int right)
         this->helper_merge(mid + 1, right);
         this->merge(left, mid, right);
     }
+}
+
+
+void PmergeMe::create_dq()
+{
+    for (int i = 0; i < (int)pairs_dq.size(); i++)
+    {
+        this->chain_dq.push_back(pairs_dq[i].first);
+        this->pend_dq.push_back(pairs_dq[i].second);
+    }
+    if (pend_dq.size() > 0)
+        chain_dq.insert(chain_dq.begin(), pend_dq[0]);
 }
 
 void PmergeMe::create()
@@ -106,36 +211,89 @@ void PmergeMe::generateJacobsthal(int n)
     return ;
 }
 
+void PmergeMe::generateJacobsthal_dq(int n)
+{
+    if (n <= 0)
+        return ;
+
+    jacob_dq.push_back(0);
+    if (n == 1)
+        return ;
+
+    jacob_dq.push_back(1);
+    int next = 0;
+    for (int i = 2; i < n; ++i)
+    {
+        next = jacob_dq[i - 1] + 2 * jacob_dq[i - 2];
+        jacob_dq.push_back(next);
+    }
+    return ;
+}
+
 void PmergeMe::combination()
 {
-    int first, last, i = 0;
+    int first, last;
 
-    while (i+1 < (int)jacob.size() - 1)
+    for (int i = 0; i < ((int)jacob.size() - 1) && comb.size() < chain.size(); i++)
     {
         first = jacob[i];
         last = jacob[i + 1];
-
         comb.push_back(jacob[i + 1]);
-        while (first < last - 1)
+       if ((last - first) > 1)
+       {
+            for (int j = last - 1; j > first; --j)
+                comb.push_back(j);
+       }
+    }
+}
+
+void PmergeMe::combination_dq()
+{
+    int first, last, i = 0;
+
+    while (i+1 < (int)jacob_dq.size() - 1)
+    {
+        first = jacob_dq[i];
+        last = jacob_dq[i + 1];
+
+        comb_dq.push_back(jacob[i + 1]);
+        while (first < last - 1 && comb_dq.size() < chain_dq.size())
         {
             last--;
-            comb.push_back(last);
+            comb_dq.push_back(last);
         }
         i++;
     }
 }
 
+
 PmergeMe::PmergeMe(char **av, int ac)
 {
+    //  ============== vector ===========
+    timeval start, end;
+
+    gettimeofday(&start, NULL);
 	
     if (this->check(av))
     {
+        std::cout << "Before: " ;
+        int j = 1;
+        while (j < ac)
+        {
+            std::cout << av[j] << " ";
+            j++;
+        }
+        std::cout << "\n";
+
         int i = 1;
         while (i + 1 < ac)
         {
             this->pairs.push_back(std::make_pair(atoi(av[i]), atoi(av[i + 1])));
 			i += 2;
         }
+
+        if (this->check_double(ac, av))
+            return ;
 		this->sort_inside_pair();
 		this->helper_merge(0, this->pairs.size() - 1);
         this->create();
@@ -157,10 +315,71 @@ PmergeMe::PmergeMe(char **av, int ac)
        }
         if ((int)chain.size() < ac - 1)
             chain.insert(chain.begin(), atoi(av[ac - 1]));
+
+        gettimeofday(&end, NULL);
+
+        std::cout << "After:  " ;
         for (int i = 0; i < (int)chain.size(); i++)
-            std::cout << chain[i] << std::endl;
+            std::cout << chain[i] << " ";
+        std::cout << "\n";
+
+        long seconds = end.tv_sec - start.tv_sec;
+        long useconds = end.tv_usec - start.tv_usec;
+        double duration = seconds * 1e6 + useconds;
+        duration = duration / 1e6;
+        std::cout << std::fixed << std::setprecision(6);
+        std::cout << "Time to process a range of 5 elements with std::vector : " << duration << " us" << "\n";
     }
+
+    //================== dque ================
+    timeval start1, end1;
+    gettimeofday(&start1, NULL);
+
+    if (this->check(av))
+    {
+        int i = 1;
+        while (i + 1 < ac)
+        {
+            this->pairs_dq.push_back(std::make_pair(atoi(av[i]), atoi(av[i + 1])));
+			i += 2;
+        }
+
+        if (this->check_double_dq(ac, av))
+            return ;
+		this->sort_inside_pair_dq();
+		this->helper_merge_dq(0, this->pairs_dq.size() - 1);
+        this->create_dq();
+        this->generateJacobsthal_dq(ac - 1);
+        if (this->jacob_dq.size() > 3)
+        {
+            this->jacob_dq.erase(jacob_dq.begin());
+            this->jacob_dq.erase(jacob_dq.begin());
+        }
+       this->combination_dq();
+       std::deque<int>::iterator it_dq;
+       for (int i = 0; i < (int)comb.size(); i++)
+       {
+           if (comb_dq[i] >= 0 && comb_dq[i] - 1 < (int)pend_dq.size())
+           {
+                it_dq = std::lower_bound(chain_dq.begin(), chain_dq.end(), pend_dq[comb_dq[i] - 1]);
+                chain_dq.insert(it_dq, pend_dq[comb_dq[i] - 1]);
+           }
+       }
+        if ((int)chain_dq.size() < ac - 1)
+            chain_dq.insert(chain_dq.begin(), atoi(av[ac - 1]));
+
+        gettimeofday(&end1, NULL);
+
+        long seconds1 = end1.tv_sec - start1.tv_sec;
+        long useconds1 = end1.tv_usec - start1.tv_usec;
+        double duration1 = seconds1 * 1e6 + useconds1;
+        duration1 = duration1 / 1e6;
+        std::cout << std::fixed << std::setprecision(6);
+        std::cout << "Time to process a range of 5 elements with std::deque : " << duration1 << " us" << "\n";
+    }
+	
 }
+
 
 
 PmergeMe::~PmergeMe()
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -4,23 +4,42 @@
 #include <vector>
 #include <utility>
 #include  <algorithm>
+#include <sys/time.h>
+#include <iomanip>
+#include <deque>
 
 class PmergeMe {
 	private:
-		std::vector<std::pair<int,int> > pairs;
-		std::vector<int> chain;
-		std::vector<int> pend;
-		std::vector<int> jacob;
-		std::vector<int> comb;
+		std::vector<std::pair<int,int> >	pairs;
+		std::vector<int>					chain;
+		std::vector<int> 					pend;
+		std::vector<int> 					jacob;
+		std::vector<int> 					comb;
+
+
+		std::deque<std::pair<int, int> >	pairs_dq;
+		std::deque<int> 				jacob_dq;
+		std::deque<int> 				chain_dq;
+		std::deque<int> 				pend_dq;
+		std::deque<int>					comb_dq;
 public:
 	PmergeMe(char **av, int ac);
 	int		check(char **av);
+	int		check_double(int ac, char **av);
 	void	sort_inside_pair();
 	void 	helper_merge(int left, int right);
 	void 	merge(int left, int mid, int right);
 	void	generateJacobsthal(int n);
 	void 	create();
 	void	combination();
+
+	int		check_double_dq(int ac, char **av);
+	void	sort_inside_pair_dq();
+	void	helper_merge_dq(int left, int right);
+	void	merge_dq(int left, int mid, int right);
+	void	create_dq();
+	void 	generateJacobsthal_dq(int n);
+	void	combination_dq();
 	~PmergeMe();
 };
 
